130eb42029423107a5ae78c17ade240584d0ea96
Callum <callumacourtt@gmail.com>
2023-08-24 18:33:28 +0100

    solved problem 2

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function sumRange(num) {
 
 const output = sumRange(3);
 console.log(output);
+
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * power(base, exponent - 1);
+}
+
+console.log(power(2, 4));
